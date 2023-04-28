392612ccf2d9dc8e509a29dbc5c726c5b35ba12c
임은호 <>
2023-04-28 21:08:09 +0900

    modified mongodb server

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/assignLv1")
+    .connect("mongodb://localhost:27017/assignLv1")
     .catch(err => console.log(err));
 };
 
